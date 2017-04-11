8f81d1e5552c9894e77dc44483009ee27f779c49
Xu Liang <obdark1988@gmail.com>
2017-04-11 16:37:31 -0500

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+return [
+	'sparrow' => [
+		'credit_private_key' => '',
+                'ach_private_key' => '',
+		'public_key'  => ''
+	],
+	'quickbooks' => [
+		'company_id' => '',
+		'app_token'  => '',
+		'oauth_key'  => '',
+		'oauth_secret_key' => ''
+	],
+	'mysql' => [
+		'host' => '',
+		'user' => '',
+		'pass' => '',
+		'dbname' => ''
+	]
+];
